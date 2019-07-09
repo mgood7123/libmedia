36b32a6bd092167f6adc17ad0a73cd3e705e388c
mgood7123 <smallville7123@gmail.com>
2019-07-09 13:39:53 +1000

    add git repo https://github.com/jniemann66/ReSampler in libmedia/ReSampler add listner API in commit https://github.com/mgood7123/libmedia/commit/b3e807e31b7af04e604bc40b025ea841f8447590

--- app/src/main/java/libmedia/Oboe/Player/init.h
+++ app/src/main/java/libmedia/Oboe/Player/init.h
@@ -23,7 +23,7 @@ NATIVE(void, Oboe, Init)(JNIEnv *env, jobject type, jint sampleRate, jint frames
     oboe::DefaultStreamValues::FramesPerBurst = (int32_t) framesPerBurst;
     streamBuilder.setDirection(oboe::Direction::Output);
     // PerformanceMode MUST not be set to LowLatency in order to interact with external processors such as ViPER4Android
-    // however Oboe currently crashes if it is not set to LowLatency
+    // however Oboe (or something else) currently crashes if it is not set to LowLatency
     streamBuilder.setPerformanceMode(oboe::PerformanceMode::LowLatency);
     streamBuilder.setSharingMode(oboe::SharingMode::Shared);
     streamBuilder.setFormat(oboe::AudioFormat::I16);
