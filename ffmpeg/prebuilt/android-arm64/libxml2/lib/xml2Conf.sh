afc356d7fbba49a50418905de20013310f27427f
Matthew Good <smallville7123@gmail.com>
2020-05-16 22:49:26 +1000

    added ffmpeg prebuilt folder

---
+++ ffmpeg/prebuilt/android-arm64/libxml2/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/mac/StudioProjects/libmedia-Upated/ffmpeg/prebuilt/android-arm64/libxml2/lib"
+XML2_LIBS="-lxml2 -L/Users/mac/Library/Android/sdk/ndk-bundle/platforms/android-24/arch-arm64/usr/lib -lz   -L/Users/mac/StudioProjects/libmedia-Upated/ffmpeg/prebuilt/android-arm64/libiconv/lib/lib  -lm "
+XML2_INCLUDEDIR="-I/Users/mac/StudioProjects/libmedia-Upated/ffmpeg/prebuilt/android-arm64/libxml2/include/libxml2 -I/Users/mac/StudioProjects/libmedia-Upated/ffmpeg/prebuilt/android-arm64/libiconv/lib/include"
+MODULE_VERSION="xml2-2.9.10"
+
